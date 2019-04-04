9216af48b66cd1718e81f71b98113954b4701630
sakshi mehta <c3330509@uon.edu.au>
2019-04-04 06:50:47 +0000

    Created an array for str[80]. Introduced the function 'fgets' to make the program read whitespace (however this is currently not working).
    Then gave the value of string length to a variable 'n'.  After created a for loop for another variable 'i', which converts the lower case letters to upper case, using an if statement.
    
    Then printed the value of 'n'.

--- project.c
+++ project.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () {
+    
+    char str[80];
+    int n, i; 
+    printf ("ENTER THE MESSAGE HERE\n");
+    fgets(str, 80, stdin); //does read the whitespaces instead of exiting after the whitespace for string. 
+    
+    n = strlen(str);
+    
+    for(i=0; i < n; i++)
+    {
+        if (str[i] >= 97  && str [i] <= 122 )
+        str [i] = str [i] - 32;
+        
+        printf ("SENTENCE IN UPPER CASE IS  %d\n", n); 
+    }
+   // printf ("SENTENCE IN UPPER CASE IS\n", str[i]);
 
-return 0;
-
+    return 0; 
+    
 }
